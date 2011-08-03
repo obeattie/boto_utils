7d78b3485b74218448ffe4b07819c1764c154d18
Oliver Beattie <oliver@obeattie.com>
2011-08-03 09:43:24 +0100

    Use setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='boto_utils',
@@ -7,11 +7,21 @@ setup(
     long_description=open('description.rst', 'r').read(),
     author='Oliver Beattie',
     author_email='oliver@obeattie.com',
-    url='https://github.com/obeattie/boto-ses-tools',
+    url='https://github.com/obeattie/boto_utils',
     packages=['boto_utils'],
+    install_requires=['boto>=2.0'],
     scripts=[
         'boto_utils/ses-send-email',
         'boto_utils/ses-verify-email-address',
         'boto_utils/s3-put',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
     ]
 )
