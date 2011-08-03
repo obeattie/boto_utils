ca2f72b787711355852e1830f65a9b334cfb5d1b
Oliver Beattie <oliver@obeattie.com>
2011-08-03 09:52:55 +0100

    Maybe don't use setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name='boto_utils',
@@ -9,7 +9,6 @@ setup(
     author_email='oliver@obeattie.com',
     url='https://github.com/obeattie/boto_utils',
     packages=['boto_utils'],
-    install_requires=['boto>=2.0'],
     scripts=[
         'boto_utils/ses-send-email',
         'boto_utils/ses-verify-email-address',
