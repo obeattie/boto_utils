3d9fb92716828e1a3afeb38316d18bcf46715350
Oliver Beattie <oliver@obeattie.com>
2011-08-19 09:47:33 +0100

    Adding glob support for s3-put

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='boto_utils',
-    version='0.1.3',
+    version='0.1.4',
     description='Command-line tools based on Boto',
     long_description=open('README.txt', 'r').read(),
     author='Oliver Beattie',
