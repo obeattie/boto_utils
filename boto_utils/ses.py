ad5343508355af3721accd0252f4d1f221f1e17b
Oliver Beattie <oliver@obeattie.com>
2011-12-06 12:31:29 +0000

    Resolve host issue

--- boto_utils/ses.py
+++ boto_utils/ses.py
@@ -1,15 +1,14 @@
 import re
 
+from boto.regioninfo import RegionInfo
 from boto.ses.connection import SESConnection
-from boto.ses import get_region
+
 from boto_utils.common import parse_aws_credentials_file
 
 def get_ses_connection(args):
-	"""Given a set of parsed arguments, returns an SESConnection."""
-	credentials = parse_aws_credentials_file(args.credentials_file)
-	
-	return SESConnection(
-        host=args.host,
-        debug=(2 if args.verbose else 0),
-		**credentials
-    )
+    """Given a set of parsed arguments, returns an SESConnection."""
+    credentials = parse_aws_credentials_file(args.credentials_file)
+    region = RegionInfo(endpoint=args.host)
+    connection = SESConnection(debug=(2 if args.verbose else 0), **credentials)
+    region.connection = connection
+    return connection
