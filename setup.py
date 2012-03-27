7bf6d633402798ac5ba96b790c1a98ef1b2c16e1
Oliver Beattie <oliver@obeattie.com>
2012-03-27 14:17:30 +0100

    Added an s3-delete command

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@ from setuptools import setup
 
 setup(
     name='boto_utils',
-    version='0.1.7',
+    version='0.2',
     description='Command-line tools based on Boto',
     long_description=open('README.rst', 'r').read(),
     author='Oliver Beattie',
     author_email='oliver@obeattie.com',
     url='https://github.com/obeattie/boto_utils',
     packages=['boto_utils'],
-    install_requires=['boto>=2.0'],
+    install_requires=['boto>=2.2.0'],
     scripts=[
         'boto_utils/ses-send-email',
         'boto_utils/ses-verify-email-address',
         'boto_utils/s3-put',
         'boto_utils/s3-geturl',
         'boto_utils/s3-copy',
+        'boto_utils/s3-delete',
     ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
